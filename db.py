34fc0c2758fa9322a2878fed711013646e070bca
sandun-prabashana <sandunprabashana@gmail.com>
2022-07-15 11:32:41 +0530

    chnage db

--- db.py
+++ db.py
@@ -3,13 +3,13 @@ from flaskext.mysql import MySQL
 
 mysql = MySQL()
 
-# application.config['MYSQL_DATABASE_USER'] = 'admin'
-# application.config['MYSQL_DATABASE_PASSWORD'] = 'admin123'
-# application.config['MYSQL_DATABASE_DB'] = 'happygas'
-# application.config['MYSQL_DATABASE_HOST'] = 'happygas.cye7dm9aermq.us-west-2.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_USER'] = 'admin'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'admin123'
+app.config['MYSQL_DATABASE_DB'] = 'happygas'
+app.config['MYSQL_DATABASE_HOST'] = 'happygas.cye7dm9aermq.us-west-2.rds.amazonaws.com'
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'mysql'
-app.config['MYSQL_DATABASE_DB'] = 'HappyGas'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'mysql'
+# app.config['MYSQL_DATABASE_DB'] = 'HappyGas'
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
